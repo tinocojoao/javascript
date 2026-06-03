2ea72cb377bdfc1450bbccd673d56c2eff1f9980
João Tinoco <jtinoco.morais@gmail.com>
2026-06-03 18:31:36 -0300

    aula13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+while (c <= 6){
+    console.log('tudo bem?')
+    c++ // c = c + 1 
+}
+
+
+
+
+
+
+
+
+
+/* console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?') */
