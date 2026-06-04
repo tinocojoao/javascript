9dbaaeedb57d8d939c8ac1891e389b3c81fede9d
João Tinoco <jtinoco.morais@gmail.com>
2026-06-04 17:32:22 -0300

    ajustes ex014

--- pratica/ex014/modelo/script.js
+++ pratica/ex014/modelo/script.js
@@ -1,32 +1,29 @@
-var horario = document.getElementById('horario')
-
-var data = new Date() // função date
-var hora = data.getHours() // qual informação quero da função
-
-horario.innerHTML = (`Agora são ${hora} horas.`) // horario na tela
-var res = document.getElementById('res') // container da img
-
-var img = document.createElement('img') // criei uma tag img
-img.setAttribute('id', 'foto') // uma id para a tag
-
 function logica(){
+    var msg = document.getElementById('msg')
+    var res = document.getElementById('res') // CONTAINER DIV DA IMG
+    var img = document.createElement('img') // CRIEI A TAG IMG MAS ELA N ESTA NO HTML AINDA
+    img.setAttribute('id', 'foto') // DEFINO UMA ID PARA IMG
+    var data = new Date() 
+    var hora = data.getHours() 
+    //hora =15
+    msg.innerHTML = (`Agora são ${hora} horas.`) 
+
     if(hora < 6){ 
         // BOA MADRUGADA
         document.body.style.backgroundColor = '#655b52'
-        img.setAttribute('src', 'imagens/foto-madrugada.png')
+        img.src = 'imagens/foto-madrugada.png' // OPÇÃO MENOR
         } else if(hora < 12){
         // BOM DIA
             document.body.style.backgroundColor = '#e2d9bc'
-            img.setAttribute('src', 'imagens/foto-manha.png')
+            img.src = 'imagens/foto-manha.png'
         } else if(hora < 18){
         // BOA TARDE
             document.body.style.backgroundColor = "#ca6e01"
-            img.setAttribute('src', 'imagens/foto-tarde.png')
+            img.setAttribute('src', 'imagens/foto-tarde.png') //OPÇÃO MAIOR
         } else{
         // BOA NOITE
             document.body.style.backgroundColor = '#0b111d'
             img.setAttribute('src', 'imagens/foto-noite.png')
         }
+       res.appendChild(img) // COLOCA A IMAGEM NO HTML
 }
-
-res.appendChild(img)
